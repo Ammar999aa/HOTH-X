f96cbb20c768750e5846a6366a1dac130dfd4813
lamartuker <108063423+lamartuker@users.noreply.github.com>
2023-03-05 17:50:12 -0800

    fixed the duration time
    switched from time_t to string

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 #include<vector>
 #include<algorithm>
 #include<ctime>
+#include<cstring>
 #include<sstream>
+
 using namespace std;
 
 int main() {
-    string start, end, car, plane;
-      time_t tdate,rdate;
+    string start, end, car, plane, timestr,timestr2;
+      string tdate,rdate;
 	vector<string> trans;
     
 	cout << "Welcome to Boujie Budget!"<<endl;
@@ -17,24 +19,43 @@ int main() {
 	getline(cin,start, '\n');
 	cout << "Please enter your drop off location: ";
 	getline(cin, end, '\n');
-    cout << "Please enter the date of travel (YYYY-MM-DD): ";
-    cin >> tdate;
-    struct tm tm{};
-   // tdate== mktime(strptime(tdate, "%Y-%m-%d", &tm));
-  
+   cout << "Please enter the date of travel (YYYYMMDD): ";
+  cin >> tdate;
+  //  //getline(cin, timestr);
+  //  
+  // struct tm tm{};
+  // memset(&tm, 0, sizeof(struct tm));
+  // // strptime(timestr.c_str(), "%Y-%m-%d", &tm);
+  // // std::tm tm = {};
+  ////  time_t date_time = std::mktime(&tm);
+  //  
+  ////  cout << "Date as time_t: " << date_time << std::endl;
+   cout << "Please enter the date of return (YYYY-MM-DD): ";
+  cin >> rdate; 
+  // double duration = difftime(rdate, tdate);
+  // 
+  //duration /= (60 * 60 * 24);
+  //  cout << "Duration of stay: " << duration << " days." << endl;
    
-    cout << "Please enter the date of return (YYYY-MM-DD): ";
-    cin >> rdate; 
-    double duration = difftime(tdate, rdate); duration /= (60 * 60 * 24);
-    cout << "Duration of stay: " << duration << " days." << endl;
-    std::string ans;
-    std::cin >> ans;
+    std::tm tm1 = {}, tm2 = {};
+    tm1.tm_year = std::stoi(tdate.substr(0, 4)) - 1900;
+    tm1.tm_mon = std::stoi(tdate.substr(4, 2)) - 1;
+    tm1.tm_mday = std::stoi(tdate.substr(6, 2));
+    tm2.tm_year = std::stoi(rdate.substr(0, 4)) - 1900;
+    tm2.tm_mon = std::stoi(rdate.substr(4, 2)) - 1;
+    tm2.tm_mday = std::stoi(rdate.substr(6, 2));
+    std::time_t t1 = std::mktime(&tm1);
+    std::time_t t2 = std::mktime(&tm2);
+
+    double duration = difftime(t2, t1);
+    duration /= (60 * 60 * 24);
+    std::cout << "Duration of stay: " << duration << " days." << std::endl;
     cout << "Please select  your deired means of transportation: "<< endl;
     std::vector<std::string> userInputs; // declare a vector to store user input
     cout << "A) car  B) bus  C) plane "<<endl;
-    
+     std::string ans;
+    std::cin >> ans;
 
-    
     trans.push_back(ans); // store user input in vector
 
     if (ans == "A") {
@@ -49,6 +70,19 @@ int main() {
         userInputs.push_back(std::to_string(carYear)); // store user input in vector
 
         std::cout << "Your preferred means of transportation is a " << carYear << " " << carType << "." << std::endl;
+    }
+    else if (ans == "B") {
+        
+    }
+    else if (ans == "C") {
+
+    }
+    else {
+        cout << "Input is invalid. Please try again.";
+        cout << "Please select  your deired means of transportation: " << endl;
+        std::vector<std::string> userInputs; // declare a vector to store user input
+        cout << "A) car  B) bus  C) plane " << endl;
+        std::cin >> ans;
     }
 
    
